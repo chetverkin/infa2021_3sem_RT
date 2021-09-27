29fd3a51c13024d5b20ef01faaa6dc5e9173f3b8
Danil <dududugin@GURAM.local>
2021-09-27 06:12:41 +0300

    re done

--- dz1/abacaba.c
+++ dz1/abacaba.c
@@ -4,12 +4,11 @@
 #include <stdlib.h>
 
 
-void GeneateString(int n){
+void GeneateString(int n, char* string1){
 	// Преобразование первого символа в код
 	char* letter = "a";
 	int letterСode = (int)letter[0];
 	// Выделение памяти
-	char *string1 = malloc((n*n*n)*sizeof(char));
 	char *string2 = malloc((n*n*n)*sizeof(char));
 	string1[0] = letter[0];
 	// Печать строк
@@ -19,9 +18,9 @@ void GeneateString(int n){
 		letterСode+=1;
 		string1[strlen(string1)] = (char)(letterСode);
 		string1 = strcat(string1, string2);
-		printf("%s\n", string1);
 		}
-	
+	free(string2);
+	string2 = NULL;
 	}
 
 
@@ -30,8 +29,14 @@ void GeneateString(int n){
 
 
 int main(int argc, char **argv)
-{
-	GeneateString(8);
+{    
+	int n = 8;
+	char *string1 = malloc((n*n*n)*sizeof(char));
+	GeneateString(n, string1);
+	printf("%s\n", string1);
+	free(string1);
+	string1 = NULL;
+	
 	return 0;
 }
 
